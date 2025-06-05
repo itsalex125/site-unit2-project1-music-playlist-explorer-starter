f99f10a65f600e9e08315e45d4d6fc88c2992176
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-05 12:02:27 -0700

    Changing modal

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,48 @@
+let playlists = [{
+    "playlistID": 1,
+    "playlist_name": "r-and-b",
+    "playlist_author": "Jeff",
+    "songs": 3},
+    {
+    "playlistID": 2,
+    "playlist_name": "lofi-beats",
+    "playlist_author": "Anna",
+    "songs": 12
+    },
+    { 
+    "playlistID": 3,
+    "playlist_name": "90s-rock",
+    "playlist_author": "Mike",
+    "songs": 8
+    },
+    {
+    "playlistID": 4,
+    "playlist_name": "jazz-nights",
+    "playlist_author": "Sophia",
+    "songs": 15
+    },
+    {
+    "playlistID": 5,
+    "playlist_name": "pop-hits",
+    "playlist_author": "Carlos",
+    "songs": 20
+    },
+    {
+    "playlistID": 6,
+    "playlist_name": "classical-vibes",
+    "playlist_author": "Emily",
+    "songs": 9
+    },
+    {
+    "playlistID": 7,
+    "playlist_name": "edm-energy",
+    "playlist_author": "Liam",
+    "songs": 17
+    },
+    {
+    "playlistID": 8,
+    "playlist_name": "country-roads",
+    "playlist_author": "Megan",
+    "songs": 5
+    }
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,47 @@
+function openModal(clickedImage){
+    let modal = document.querySelector(".playlistModal");
+    let modalimg = document.querySelector
+}
 
+img.forEach(img =>{
+    img.addEventListener("click", () =>{
+        modal.style.display = "block";
+    });
+});
+
+closeBtn.onclick = () =>{
+    modal.style.display = "none";
+};
+
+modal.onclick = (e) =>{
+    if(e.target === modal) modal.style.display = "none";
+};
+
+const loadPlaylist = () => {
+    console.log('loading Playlist');
+    const container = document.querySelector(".playlist-cards");
+    for(const pl of playlists){
+        const elem = createPlaylist(pl);
+        container.appendChild(elem);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadPlaylist();
+});
+
+const createPlaylist=(playlist)=>{
+    console.log(playlist);
+    const playlistElement = document.createElement('article');
+    playlistElement.className = 'playlist' ;
+    playlistElement.innerHTML = `
+            <article class = "playlist">
+                <img id="holder" src="/music-playlist-creator/assets/img/playlist.png">
+                <article class = "text">
+                <h3>${playlist.playlist_name}</h3>
+                <p>${playlist.playlist_author}</p>
+                <img id= "heart" src="/music-playlist-creator/assets/img/heart.jpg">
+                </article>
+            </article>`;
+    return playlistElement;
+}
